e5b483a8505f1a07338b84d964d31d62cbda9d1f
mw <maciej.wojton@gmail.com>
2025-04-13 22:11:31 -0400

    1539

---
+++ exercises/1539_missing _num.py
@@ -0,0 +1,32 @@
+
+
+from typing import List
+
+
+class Solution:
+    def find(self,nums,k):
+        lo,hi=0,len(nums)-1
+        while lo<=hi:
+            mid=lo+(hi-lo)//2
+            missing = nums[mid]-mid-1
+            if missing < k:
+                lo=mid+1
+            else:
+                hi=mid-1
+        return lo+k
+
+s=Solution()
+print(s.find([1,2,3,4],3))
+
+#variant LC1060, missing from the starting index
+class Solution2:
+    def missingElement(self, nums: List[int], k: int) -> int:
+        lo,hi=0,len(nums)-1
+        while lo<=hi:
+            mid=lo+(hi-lo)//2
+            target=nums[mid]-mid-nums[0]
+            if target < k:
+                lo=mid+1
+            else:
+                hi=mid-1
+        return hi+k+nums[0]
